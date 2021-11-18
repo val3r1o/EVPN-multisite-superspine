eb5e02b3f98104aafe7e983343baed14c1b5d551
Valerio Martini <valerio.martini@gmail.com>
2021-11-18 17:05:45 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,8 @@ echo ------------------ This is an automatic backup ----------------------------
 echo ------------------------------------------------------------------------------------------------------
 echo ------------------------------------------------- Dell NETWORKING ------------------------------------
 echo ------------------------------------------------------------------------------------------------------
-
+</br>
+</br>
 cd EVPN-multisite-superspine/
 ansible-playbook -i inventory copyconfig.yml
 git status
@@ -16,13 +17,11 @@ git push origin main
 git status
 
 echo ----------- This is an automatic backup -------------------------------------------------------------
-echo - update                                                                                          ---
-echo ---- please note on git auth changes August 2021                                                  ---
-echo - single password not accepted anymore                                                            ---
-echo - setup your token                                                                                ---
-echo - https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
-echo - remember to move from SSH to HTTPS remote git                                                   ---
-echo - https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories    ---
 echo------------------------------------------------------------------------------------------------------
 echo ---------------------------------------------- Dell NETWORKING --------------------------------------
 echo --------------------------valerio.martini@gmail.com--------------------------------------------------
+echo------------------------------------------------------------------------------------------------------
+
+echo ---- please note on git auth changes August 2021 - single password not accepted anymore
+echo - setup your token
+echo - https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
